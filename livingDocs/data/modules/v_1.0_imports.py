c5fc3ac4949699f9b509a1fec7cf8ff64926fc19
inkedYogi <abenedict85@gmail.com>
2023-12-29 18:37:27 -0800

    Create v_1.0_imports.py
    import statements for Advanced_Math_Module, Nuclear_Chemistry_Module, DNA_Module, ND_Plotting_Module, and Core_Module (all v1.0)

---
+++ livingDocs/data/modules/v_1.0_imports.py
@@ -0,0 +1,20 @@
+import os
+import math
+import yaml
+import json
+import time
+import base64
+import random
+import requests
+import itertools
+import numpy as np
+import pandas as pd
+from clifford import Cl
+from random import randint
+from datetime import datetime
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
+from IPython.display import Audio
+from scipy.special import jv, airy
+from IPython.display import clear_output
