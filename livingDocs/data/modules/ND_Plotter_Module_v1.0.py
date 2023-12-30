d8588cd9652c594c005ef1a4959cf909da325bbb
inkedYogi <abenedict85@gmail.com>
2023-12-29 18:39:18 -0800

    Create ND_Plotter_Module_v1.0.py
    omit imports

---
+++ livingDocs/data/modules/ND_Plotter_Module_v1.0.py
@@ -0,0 +1,51 @@
+# ND_Plotter_Module v1.0
+
+def makeYaml(data, plot_title = 'Default Name', yaml_filename='plot_style.yaml'):
+    default_style = {
+        'figure_size': [6, 4],
+        'plot_type': 'line',  # Default plot type
+        'x_column': data.columns[0],
+        'y_column': data.columns[1],
+        'x_label': data.columns[0],
+        'y_label': data.columns[1],
+        'title': plot_title,
+        'grid': True
+    }
+
+    with open(yaml_filename, 'w') as file:
+        yaml.dump(default_style, file)
+
+    print(f"Default YAML file '{yaml_filename}' created.")
+
+def prepare_data(file_name):
+    if file_name.endswith('.csv'):
+        data = pd.read_csv(file_name)
+    elif file_name.endswith('.txt'):
+        # Add logic for .txt file processing
+        data = pd.read_table(file_name)
+    elif file_name.endswith('.md'):
+        # Add logic for .md file processing
+        pass  # Markdown files usually don't contain plottable data
+    else:
+        raise ValueError("Unsupported file type.")
+    return data
+
+def plot_data(data, style_yaml, x_column=None, y_column=None):
+    with open(style_yaml, 'r') as file:
+        style = yaml.safe_load(file)
+
+    # Determine columns to plot
+    if x_column is None or y_column is None:
+        x_column, y_column = data.columns[:2]
+
+    plt.figure(figsize=style.get('figure_size', (10, 6)))
+    if style.get('plot_type') == 'line':
+        plt.plot(data[x_column], data[y_column])
+    elif style.get('plot_type') == 'stem':
+        plt.stem(data[x_column], data[y_column], markerfmt=style.get('markerfmt', 'o'))
+
+    plt.xlabel(style.get('x_label', x_column))
+    plt.ylabel(style.get('y_label', y_column))
+    plt.title(style.get('title', 'Plot'))
+    plt.grid(style.get('grid', True))
+    plt.show()
